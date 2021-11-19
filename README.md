5d058695d26fe72ddc95a7874528d50a634ee74b
werben <werben@qq.com>
2021-11-19 19:57:10 +0800

    modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# router
+# Router
 aws apigateway lambda router for golang
+
+
+# Example:
+```
+package main
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/werbenhu/router"
+)
+
+var r *router.Router
+
+func init() {
+	r = router.New()
+	r.Get("/test", test)
+	r.Get("/test/:name", testWithName)
+}
+
+func testWithName(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	resp := map[string]interface{}{
+		"path":   request.Path,
+		"method": request.HTTPMethod,
+		"params": request.QueryStringParameters,
+	}
+
+	body, _ := json.Marshal(resp)
+	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
+}
+
+func test(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	resp := map[string]interface{}{
+		"path":   request.Path,
+		"method": request.HTTPMethod,
+		"params": request.QueryStringParameters,
+	}
+
+	body, _ := json.Marshal(resp)
+	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(r.Handler)
+}
+
+```
--- example/main.go
+++ example/main.go
@@ -12,13 +12,19 @@ func main() {
 	r := router.New()
 
 	r.Get("/", func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-		fmt.Printf("/\n")
+		fmt.Printf("match path /\n")
+		return events.APIGatewayProxyResponse{Body: "GET WERBEN IN ROUTER", StatusCode: 200}, nil
+	})
+
+	r.Get("/ttt/", func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		fmt.Printf("match path /ttt\n")
+		fmt.Printf("params: %+v\n", request.QueryStringParameters)
 		return events.APIGatewayProxyResponse{Body: "GET WERBEN IN ROUTER", StatusCode: 200}, nil
 	})
 
 	r.Get("/ttt/:abc", func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-		fmt.Printf("/ttt\n")
-		fmt.Printf("%+v\n", request.QueryStringParameters)
+		fmt.Printf("match path /ttt rest api with value \n")
+		fmt.Printf("params: %+v\n", request.QueryStringParameters)
 		return events.APIGatewayProxyResponse{Body: "GET WERBEN IN ROUTER", StatusCode: 200}, nil
 	})
 
@@ -28,6 +34,10 @@ func main() {
 		Path:       "/",
 	}
 	ret, err := r.Handler(ctx, request)
+	fmt.Printf("ret:%+v, err:%s\n", ret, err)
+
+	request.Path = "/ttt"
+	ret, err = r.Handler(ctx, request)
 	fmt.Printf("ret:%+v, err:%s\n", ret, err)
 
 	request.Path = "/ttt/werben"
